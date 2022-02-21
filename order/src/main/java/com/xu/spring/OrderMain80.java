b6b260d69bdd9a56ad06e7b32e1bfd7d04cd8059
xuchaofeng <826639991@qq.com>
2022-02-21 14:08:26 +0800

    支付、订单微服务注册进eureka

--- order/src/main/java/com/xu/spring/OrderMain80.java
+++ order/src/main/java/com/xu/spring/OrderMain80.java
@@ -2,11 +2,13 @@ package com.xu.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * cloud
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- payment-8081/src/main/java/com/xu/spring/PaymentMain8081.java
+++ payment-8081/src/main/java/com/xu/spring/PaymentMain8081.java
@@ -2,11 +2,13 @@ package com.xu.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * cloud
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8081 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8081.class, args);
